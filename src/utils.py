131cbc97286a0ee2eb61c687d71ca89508c70f71
v.markov <21138532110q@gmail.com>
2021-06-19 20:01:44 +0500

    fixed black in src/utils.py

--- src/utils.py
+++ src/utils.py
@@ -7,4 +7,4 @@ def make_request():
     flask_settings = settings.get("flask")
 
     url = f"http://{flask_settings.get('host')}:{flask_settings.get('port')}/recognize"
-    print(requests.post(url=url, data='Hello, World'))
+    print(requests.post(url=url, data="Hello, World"))
